493117950d0fccf8d7e3eb6bfcea7d36147f174e
Maaz <maazshaikh4559@gmail.com>
2022-09-11 23:33:57 +0530

    initialized as a git repository

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        'tablet': '1150px',
+        'vl': '1400px'
+      },
+    },
+  },
+  plugins: [],
+}
